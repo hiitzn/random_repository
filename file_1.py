ffef58dfe67e0a35dfb3b218d7582180301b8d60
Anastasia Simbirewa <anastasiasimbirewa@gmail.com>
2025-09-11 22:37:06 +0300

    to rebase

--- file_1.py
+++ file_1.py
@@ -1,14 +1,9 @@
 try:
-    a = float(input("Введите ЧЕТНОЕ число a: "))  
-    b = float(input("Введите ЧЕТНОЕ число b: "))  
-    
+    a = float(input("Введите ЧЕТНОЕ число a: "))
+    b = float(input("Введите ЧЕТНОЕ число b: "))
     if a % 2 == 0 and b % 2 == 0:
-        print("Сумма чисел:", a + b)  
+        print("Сумма чисел:", a + b)
     else:
         print("Числа нечетные!")
 except ValueError:
     print("Ошибка: введите числа!")
-
-
-
-
